87330014a4786682d9e42a30c0373a5a0854358a
Lakshmi <lakshmi@gmail.com>
2022-07-28 17:58:58 +0530

    1v

---
+++ pdfconv/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PdfconvConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pdfconv'
---
+++ pdfconv/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import PyPDF2
+from gtts import gTTS
+
+
+def index(request):
+    if request.method == "POST":
+
+        print("hello")  
+       # path of the PDF file
+        path = open('pdfconv/static/pdf/file.pdf', 'rb')        
+        # creating a PdfFileReader object
+        pdfReader = PyPDF2.PdfFileReader(path)        
+        # the page with which you want to start
+        # this will read the page of 25th page.
+        text =''
+        count=pdfReader.numPages
+        for i in range(count):
+            from_page = pdfReader.getPage(i)
+            # # extracting the text from the PDF
+            text += from_page.extractText()        
+        # reading the text
+        tts = gTTS(text)
+        tts.save("pdfconv/static/audio/hi.mp3")
+    return render(request, 'index.html')
+
+def about(request):
+
+    return HttpResponse("<h1>About page</h1>")
