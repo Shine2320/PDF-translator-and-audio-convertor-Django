d0a202f38e90e177b0b11e238c691ff4264b3567
Shine Mathew <shinemathew24@gmail.com>
2022-07-28 23:42:39 +0530

    registration,login,logout completed

--- pdfconv/urls.py
+++ pdfconv/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('usersignup/',views.usersignup,name='usersignup'),
-    path('pdfconv/',views.about,name='about')
+    path('pdfconv/',views.about,name='about'),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout"),
 ]
--- pdfconv/views.py
+++ pdfconv/views.py
@@ -1,16 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 import PyPDF2
 from gtts import gTTS
-
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout 
 
 def home(request):
     return render(request,'home.html')
 
 def usersignup(request):
-    if request.method == 'POST':
-        
-    return render(request,'usersignup.html')
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("home")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render (request=request, template_name="usersignup.html", context={"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("home")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="login.html", context={"login_form":form})
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("home")
+
 
 def index(request):
     if request.method == "POST":
