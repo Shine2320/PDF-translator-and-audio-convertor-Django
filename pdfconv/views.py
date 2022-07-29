4e24776d4ee2f1dfbc328affa25eae0cec0277d0
Shine Mathew <shinemathew24@gmail.com>
2022-07-29 17:13:51 +0530

    completed main features

--- pdfconv/views.py
+++ pdfconv/views.py
@@ -55,24 +55,7 @@ def logout_request(request):
 
 
 def user_home(request):
-    if request.method == "POST":
-
-        print("hello")  
-       # path of the PDF file
-        path = open('pdfconv/static/pdf/file.pdf', 'rb')        
-        # creating a PdfFileReader object
-        pdfReader = PyPDF2.PdfFileReader(path)        
-        # the page with which you want to start
-        # this will read the page of 25th page.
-        text =''
-        count=pdfReader.numPages
-        for i in range(count):
-            from_page = pdfReader.getPage(i)
-            # # extracting the text from the PDF
-            text += from_page.extractText()        
-        # reading the text
-        tts = gTTS(text)
-        tts.save("pdfconv/static/audio/hi.mp3")
+    
     return render(request, 'user_home.html')
 def user_pdf_upload(request):
     if request.method == 'POST':  
